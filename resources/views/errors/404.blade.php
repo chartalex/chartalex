c751f10eb91294b28522073649a71cf208d5d05a
chartalex <chartalex@MacBook-Air-de-Alexandre.local>
2016-03-24 12:03:07 +0000

    add 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+
+@extends('layouts.master')
+
+@section('title', 'Login')
+
+@section('content')
+
+<br><br>
+
+<div class="row">
+    <div class="col-sm-6 col-sm-offset-3">
+        <h3 class="">404</h3>
+        <h4>Oops! This page doesn't exist!</h4>
+        <p class="small">
+            The link might be corrupted<br>
+            or the page may have been removed.
+        </p>
+        <p>
+            <i class="fa fa-home"></i><a href="/" class="text-muted"> Back to homepage</a>
+        </p>
+    </div>
+</div>
+
+@endsection
+
+@section('script')
+
+@endsection
